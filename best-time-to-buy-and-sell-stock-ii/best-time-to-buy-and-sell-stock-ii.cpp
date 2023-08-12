5fb98ecd5a02a1c81a7b366d453455705dcf1a5e
Ribhav Bansal <96466752+RibhavBansal@users.noreply.github.com>
2023-08-12 16:10:52 +0530

    Time: 4 ms, Memory: 13 MB - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int ans = 0;
+        for(int i = 1; i < a.size(); i++){
+            if(a[i] >  a[i-1]){
+                ans += (a[i]-a[i-1]);
+            }
+        }
+        return ans;
+    }
+};
+
+// https://www.youtube.com/watch?v=Q7v239y-Tik
