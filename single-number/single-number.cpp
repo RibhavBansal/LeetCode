10c0251769d191cc3324850d85f130e41d84a680
Ribhav Bansal <96466752+RibhavBansal@users.noreply.github.com>
2023-08-13 22:41:02 +0530

    Time: 20 ms, Memory: 16.8 MB - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& a) {
+        sort(a.begin(),a.end());
+        int n = a.size();
+        int k = 0;
+
+        for(int i = 0; i < a.size(); i++)
+        {
+            k ^= a[i];
+        }
+        return k;
+    }
+};
